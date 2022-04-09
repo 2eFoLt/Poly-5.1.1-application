3542c2a971d5b1b1b88870769a45c86df2bb8677
2eFoLt <grig2205@gmail.com>
2022-04-09 20:17:00 +0300

    server-upd

--- flaskServer/main.py
+++ flaskServer/main.py
@@ -3,9 +3,15 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST']
+           )
 def index():
     return render_template('home.html')
+
+
+@app.route('/secret')
+def secret():
+    return render_template('mainPage.html')
 
 
 @app.route('/login')
